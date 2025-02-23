db7719c934c58c5f840d2193b02e69c2ebd6c8f7
rgkzz <163715255+sohyun107@users.noreply.github.com>
2025-02-23 20:52:53 +0900

    Update 7576.java

--- 백준/gold/7576/7576.java
+++ 백준/gold/7576/7576.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -21,18 +20,12 @@ public class Main {
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-
-        box = new int[N + 2][M + 2];
-
-        // 외곽을 -1로 초기화
-        for (int i = 0; i < N + 2; i++) {
-            Arrays.fill(box[i], -1);
-        }
+        box = new int[N][M];
 
         boolean hasUnripe = false;
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= M; j++) {
+            for (int j = 0; j < M; j++) {
                 box[i][j] = Integer.parseInt(st.nextToken());
                 if (box[i][j] == 1) {
                     queue.offer(new int[]{i, j});
@@ -41,17 +34,16 @@ public class Main {
             }
         }
 
-        if (!hasUnripe) {   // 모든 토마토가 이미 익어 있으면
+        if (!hasUnripe) {
             System.out.println(0);
             return;
         }
 
         int days = 0;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상, 하, 좌, 우
-
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};   // 상, 하, 좌, 우
+ 
         while (!queue.isEmpty()) {
             int size = queue.size();
-
             for (int i = 0; i < size; i++) {
                 int[] current = queue.poll();
                 int x = current[0];
@@ -61,7 +53,7 @@ public class Main {
                     int nx = x + direction[0];
                     int ny = y + direction[1];
 
-                    if (box[nx][ny] == 0) {
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && box[nx][ny] == 0) {
                         box[nx][ny] = 1;
                         queue.offer(new int[]{nx, ny});   // 새롭게 익은 토마토 위치를 enqueue
                     }
@@ -72,15 +64,14 @@ public class Main {
 
         if (hasUnripeTomato()) {
             System.out.println(-1);
-        }
-        else {
+        } else {
             System.out.println(days - 1);
         }
     }
 
     static boolean hasUnripeTomato() {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 if (box[i][j] == 0) return true;
             }
         }
