46151eef9cbf70dcbac9742bf93e02d3c1036cca
rgkzz <163715255+sohyun107@users.noreply.github.com>
2025-03-01 21:44:08 +0900

    Update timeout.java

--- 백준/silver/11659/timeout.java
+++ 백준/silver/11659/timeout.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/* 이중 for문으로 인한 시간초과 */
 public class Main {
 
     public static void main(String[] args) throws IOException {
